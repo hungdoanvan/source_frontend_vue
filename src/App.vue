79bc05dde5e41aed21f53f861930f58178a14321
hungdoanvan117 <hungdoanvan117@gmail.com>
2023-11-12 12:48:33 +0700

    feat: create default layout

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,27 @@
 <template>
-  <div>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+  <DefaultLayout>
     <router-view />
-  </div>
+  </DefaultLayout>
 </template>
 
+<script lang="ts">
+import DefaultLayout from "@/views/layout/Default.vue";
+
+export default {
+  components: {
+    DefaultLayout,
+  },
+};
+</script>
+
 <style>
-
 @tailwind base;
-
 @tailwind components;
-
 @tailwind utilities;
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url("https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700");
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  font-family: Poppins, sans-serif;
 }
 </style>
